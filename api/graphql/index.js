dd6f83916ce37476b19ef284426e9d23a7ba8f43
Mark Wen <markwenguojie94@gmail.com>
2018-07-27 15:01:51 -0400

    Setting up Mutations

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -4,11 +4,12 @@ import Contribution from './Contribution.gql'
 import Repository from './Repository.gql'
 import User from './User.gql'
 import Query from './Query.gql'
+import Mutation from './Mutation.gql'
 
 import logger from '../utils/logger'
 
 export default makeExecutableSchema({
-    typeDefs: [ Contribution.typeDef, Repository.typeDef, User.typeDef, Query ],
+    typeDefs: [ Contribution.typeDef, Repository.typeDef, User.typeDef, Query, Mutation ],
     resolvers: [ Contribution.resolvers, Repository.resolvers, User.resolvers ],
     logger
 })
